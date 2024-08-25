01a70062856162b5a1ebf9b3b285a32fd38bbc9b
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-08-25 19:44:58 +0100

    first commit

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBJXd4kAqryt0eeYlkVqVBDXw4h1-Bpdjs",
+    authDomain: "creatives-7c094.firebaseapp.com",
+    projectId: "creatives-7c094",
+    storageBucket: "creatives-7c094.appspot.com",
+    messagingSenderId: "976415938757",
+    appId: "1:976415938757:web:fb2e03d99a9c50bf52b8ac",
+    measurementId: "G-WVS958PHLE"
+}
+
+export default firebaseConfig
